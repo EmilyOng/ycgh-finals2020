01bc4d92b6a863979486e73d97c9ad6811f31cb4
Emily Ong <emilyohq11@gmail.com>
2020-05-02 15:17:15 +0800

    finished basic functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session
 import matplotlib.pyplot as plt
+import textblob
 
 def get_age (age):
   '''
@@ -16,31 +17,84 @@ def get_age (age):
   elif 25 < age <= 65:return "Working Adults"
   else:return "Seniors"
 
+
+def get_category (sentiment_metric):
+  '''
+  Sentiment Category Sentiment_Metric (S) Range
+  Very Negative S ≤ 2.5
+  Slightly Negative 2.5 < S ≤ 4.5
+  Neutral 4.5 < S ≤ 6.5
+  Slightly Positive 6.5 < S ≤ 8.5
+  Very Positive S > 8.5
+  '''
+  if sentiment_metric <= 2.5:return "Very Negative"
+  elif 2.5 < sentiment_metric <= 4.5:return "Slightly Negative"
+  elif 4.5 < sentiment_metric <= 6.5:return "Neutral"
+  elif 6.5 < sentiment_metric <= 8.5:return "Positive"
+  else:return "Very Positive"
+
+
+def process_data (filename):
+  with open (f"static/data/{filename}.csv", "r") as people_responses_file:
+    people_responses = people_responses_file.readlines()
+
+  # headers: Date_YYYYMMDD,Statement,Age,Sentiment_Metric
+  people_responses = [response.rstrip() for response in people_responses]
+
+  for i in range (1, len(people_responses)):
+    temp = people_responses[i].split(",")
+    date = temp[0]
+    if filename != "people_responses_untagged":
+      sentiment_metric = float(temp[-1])
+      age = int(temp[-2])
+      statement = temp[1:len(temp)-2]
+    else:
+      age = int(temp[-1])
+      statement = temp[1:len(temp)-1]
+    if filename != "people_responses_untagged":
+      people_responses[i] = [date, statement, age, sentiment_metric]
+    else:
+      people_responses[i] = [date, statement, age]
+  return people_responses
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "984RJOLM3QD;;;.RC;W"
 
 @app.route("/")
 def index ():
-  with open ("static/data/people_responses_tagged.csv", "r") as people_responses_tagged_file:
-    people_responses_tagged = people_responses_tagged_file.readlines()
+  people_responses_tagged = process_data("people_responses_tagged")
+  people_responses_untagged = process_data("people_responses_untagged")
+  people_responses_untagged[0] = people_responses_untagged[0].split(",")
 
-  # headers: Date_YYYYMMDD,Statement,Age,Sentiment_Metric
-  people_responses_tagged = [response.rstrip() for response in people_responses_tagged]
-  for i in range (1, len(people_responses_tagged)):
-    temp = people_responses_tagged[i].split(",")
-    date = temp[0]
-    sentiment_metric = float(temp[-1])
-    age = int(temp[-2])
-    statement = temp[1:len(temp)-2]
-    people_responses_tagged[i] = [date, statement, age, sentiment_metric]
+  people_responses_untagged[0].append("Sentiment_Metric")
+  for i in range (1, len(people_responses_untagged)):
+    # print(people_responses_untagged[i][1][0].rstrip())
+    text = people_responses_untagged[i][1][0].rstrip()
+    response = textblob.TextBlob(text)
+    polarity = response.sentiment.polarity
+    # -1 to 1 => 1 to 10
+    # m = 4.5, c = 5.5 ; y= mx + c
+    sentiment_metric = 4.5 * polarity + 5.5
+    sentiment_metric = round(sentiment_metric, 2)
+    people_responses_untagged[i].append(sentiment_metric)
 
-    # calculate average sentiment across users
-    average_sentiment = 0
-    number_of_responses = len(people_responses_tagged) - 1
+
+  ### DATA PROCESSING ####
+
+  def get_percentage_of_negative_sentiment (data):
+    number_of_responses = len(data) - 1
     # negative sentiment_metric
     negative_sentiment_threshold = 5.5
     percentage_of_negative_sentiment = 0
+    for i in range (1, len(data)):
+      sentiment_metric = float(data[i][3])
+      percentage_of_negative_sentiment += sentiment_metric <= negative_sentiment_threshold
 
+    percentage_of_negative_sentiment = round(percentage_of_negative_sentiment/number_of_responses * 100, 2)
+    return percentage_of_negative_sentiment
+
+  def get_sentiment_by_age (data):
+    number_of_responses = len(data) - 1
     # calculates and summarises the average (mean) sentiment by age groups
     sentiment_by_age = {"Children": {"population": 0, "avg_sentiment_metric": 0},
                         "Teens": {"population": 0, "avg_sentiment_metric": 0},
@@ -48,40 +102,60 @@ def index ():
                         "Working Adults": {"population": 0, "avg_sentiment_metric": 0},
                         "Seniors": {"population": 0, "avg_sentiment_metric": 0}}
 
-    for i in range (1, len(people_responses_tagged)):
-      sentiment_metric = float(people_responses_tagged[i][3])
-      age = int(people_responses_tagged[i][2])
-      average_sentiment += sentiment_metric
-      percentage_of_negative_sentiment += sentiment_metric <= negative_sentiment_threshold
+    for i in range (1, len(data)):
+      sentiment_metric = float(data[i][3])
+      age = int(data[i][2])
       sentiment_by_age[get_age(age)]["avg_sentiment_metric"] += sentiment_metric
       sentiment_by_age[get_age(age)]["population"] += 1
 
-    # get the mean sentiment
-    plt_age_group = []
-    plt_mean_sentiment = []
     for age_group in sentiment_by_age:
-      plt_age_group.append(age_group)
       if sentiment_by_age[age_group]["population"] == 0: # handle ZeroDivisionError
-        plt_mean_sentiment.append(0)
         continue
+      sentiment_by_age[age_group]["avg_sentiment_metric"] = sentiment_by_age[age_group]["avg_sentiment_metric"]/sentiment_by_age[age_group]["population"]
+      sentiment_by_age[age_group]["avg_sentiment_metric"] = round(sentiment_by_age[age_group]["avg_sentiment_metric"], 2)
+    return sentiment_by_age
 
-      sentiment_by_age[age_group]["avg_sentiment_metric"] /= sentiment_by_age[age_group]["population"]
-      sentiment_by_age[age_group]["avg_sentiment_metric"] = round(sentiment_by_age[age_group]
-      ["avg_sentiment_metric"], 2)
-      plt_mean_sentiment.append(sentiment_by_age[age_group]["avg_sentiment_metric"])
+  def get_average_sentiment (data):
+    number_of_responses = len(data) - 1
+    # calculate average sentiment across users
+    average_sentiment = 0
+    for i in range (1, len(data)):
+      sentiment_metric = float(data[i][3])
+      average_sentiment += sentiment_metric
 
     average_sentiment /= number_of_responses
     average_sentiment = round(average_sentiment, 2)
-    percentage_of_negative_sentiment = round(percentage_of_negative_sentiment/number_of_responses * 100, 2)
 
-  # graph out the mean sentiment by age group
-  plt.bar(plt_age_group, plt_mean_sentiment)
-  plt.savefig("static/img/32-Sentiment by Age Group.png")
-  plt.close()
+    return average_sentiment
+
+  def get_sentiment_by_category (data):
+    number_of_responses = len(data) - 1
+    sentiment_by_category = {"Very Negative": 0,
+                            "Slightly Negative": 0,
+                            "Neutral": 0,
+                            "Positive": 0,
+                            "Very Positive": 0}
+    for i in range (1, len(data)):
+      sentiment_metric = float(data[i][3])
+      sentiment_by_category[get_category(sentiment_metric)] += 1
+
+    for category in sentiment_by_category:
+      sentiment_by_category[category] = 100 * sentiment_by_category[category] / number_of_responses
+      sentiment_by_category[category] = round(sentiment_by_category[category], 2)
+    return sentiment_by_category
+
+
+  average_sentiment = get_average_sentiment(people_responses_tagged)
+  percentage_of_negative_sentiment = get_percentage_of_negative_sentiment(people_responses_tagged)
+  sentiment_by_age = get_sentiment_by_age(people_responses_tagged)
+  sentiment_by_category = get_sentiment_by_category(people_responses_tagged)
+
   return render_template("index.html", people_responses_tagged=people_responses_tagged[1:],
+                          people_responses_untagged=people_responses_untagged[1:],
                           data = {"average_sentiment": average_sentiment,
                                   "percentage_of_negative_sentiment": percentage_of_negative_sentiment,
-                                  "sentiment_by_age": sentiment_by_age})
+                                  "sentiment_by_age": sentiment_by_age,
+                                  "sentiment_by_category": sentiment_by_category})
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
